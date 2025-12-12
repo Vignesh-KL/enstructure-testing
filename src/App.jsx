9bb059fee981f8344b745377791a3f36ec47a6c5
Vignesh Kumar L <vignesh.kumar@kaleris.com>
2025-12-12 18:48:48 +0530

    setup test app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <iframe 
+        src="https://navdmast.mtnavis.com/WebPortal"
+        width="100%"
+        height="600px"
+        title="Login Portal"
+        style={{ border: 'none' }}
+      />
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
